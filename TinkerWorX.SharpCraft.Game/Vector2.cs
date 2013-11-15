b10b97ba9931e7798dc4249ea9c5aa570b670fb4
TinkerWorX <nikolaj.mariager@gmail.com>
2013-11-15 12:53:18 +0100

    Added basic vector types.

---
+++ TinkerWorX.SharpCraft.Game/Vector2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TinkerWorX.SharpCraft.Game
+{
+    public struct Vector2
+    {
+        public Single X;
+
+        public Single Y;
+
+        public Vector2(Single x, Single y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
