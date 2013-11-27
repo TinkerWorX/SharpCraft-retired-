31fe6a9dfc2ae0d2efd8152f3958505e7653c73d
TinkerWorX <nikolaj.mariager@gmail.com>
2013-11-27 23:06:39 +0100

    Cleared out all TODO comments from Lidgren.

--- Lidgren.Network/NetPeer.MessagePools.cs
+++ Lidgren.Network/NetPeer.MessagePools.cs
@@ -197,7 +197,7 @@ namespace Lidgren.Network
 			msg.m_data = null;
 			
 			// message fragments cannot be recycled
-			// TODO: find a way to recycle large message after all fragments has been acknowledged; or? possibly better just to garbage collect them
+			// find a way to recycle large message after all fragments has been acknowledged; or? possibly better just to garbage collect them
 			if (msg.m_fragmentGroup == 0)
 				Recycle(storage);
 	
