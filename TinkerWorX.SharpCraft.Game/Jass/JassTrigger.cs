31dd539041b447babf6a02ce5700bc10c271b045
TinkerWorX <nikolaj.mariager@gmail.com>
2013-11-27 22:44:42 +0100

    Added TriggerHappy's additions to the JassTrigger struct.

--- TinkerWorX.SharpCraft.Game/Jass/JassTrigger.cs
+++ TinkerWorX.SharpCraft.Game/Jass/JassTrigger.cs
@@ -6,6 +6,14 @@ namespace TinkerWorX.SharpCraft.Game.Jass
     [JassType("Htrigger;")]
     public struct JassTrigger
     {
+        //native TriggerExecute takes trigger whichTrigger returns nothing
+        private delegate void TriggerExecutePrototype(JassTrigger whichTrigger);
+        private static TriggerExecutePrototype _TriggerExecute = WarcraftIII.Jass.GetNative("TriggerExecute").ToDelegate<TriggerExecutePrototype>();
+
+        //native TriggerEvaluate takes trigger whichTrigger returns boolean
+        private delegate JassBoolean TriggerEvaluatePrototype(JassTrigger whichTrigger);
+        private static TriggerEvaluatePrototype _TriggerEvaluate = WarcraftIII.Jass.GetNative("TriggerEvaluate").ToDelegate<TriggerEvaluatePrototype>();
+
         private readonly IntPtr Handle;
 
         public JassTrigger(IntPtr handle)
@@ -21,6 +29,16 @@ namespace TinkerWorX.SharpCraft.Game.Jass
         public String ToString(String format)
         {
             return this.Handle.ToString(format);
+        }
+
+        public JassBoolean Evaluate()
+        {
+            return _TriggerEvaluate(this);
+        }
+
+        public void Execute()
+        {
+            _TriggerExecute(this);
         }
     }
 }
