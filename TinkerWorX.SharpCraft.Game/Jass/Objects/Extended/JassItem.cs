9a4f4e34f50263c1ef65c2f66a6558bf0c30f939
TinkerWorX <nikolaj.mariager@gmail.com>
2013-12-21 22:47:31 +0100

    Added mysteriously untracked files.

---
+++ TinkerWorX.SharpCraft.Game/Jass/Objects/Extended/JassItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using TinkerWorX.SharpCraft.Game.Core;
+
+namespace TinkerWorX.SharpCraft.Game.Jass
+{
+    public partial struct JassItem
+    {
+        public CItemPtr ToCItem()
+        {
+            return CItemPtr.FromHandle(this);
+        }
+    }
+}
---
+++ TinkerWorX.SharpCraft.Game/Jass/Objects/Extended/JassTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TinkerWorX.SharpCraft.Game.Jass
+{
+    public partial struct JassTrigger
+    {
+        public JassBoolean Evaluate()
+        {
+            return WarcraftIII.Jass.Natives.TriggerEvaluate(this);
+        }
+
+        public void Execute()
+        {
+            WarcraftIII.Jass.Natives.TriggerExecute(this);
+        }
+    }
+}
