e014ff8bbae5510741b7c849f631dee1b377165c
TinkerWorX <nikolaj.mariager@gmail.com>
2014-03-11 16:38:23 +0100

    Added missing DelegateAction type.

---
+++ TinkerWorX.SharpCraft/Blizzard/Types/DelegateAction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TinkerWorX.SharpCraft.Blizzard.GameModule.Jass;
+using TinkerWorX.SharpCraft.Blizzard.GameModule.Types;
+
+namespace TinkerWorX.SharpCraft.Blizzard.Types
+{
+    public delegate void DelegateActionDelegate(JassTrigger trigger);
+
+    public delegate Boolean DelegateConditionDelegate(JassTrigger trigger);
+
+    public class DelegateAction : ManagedActionBase
+    {
+        private DelegateConditionDelegate condition { get; set; }
+
+        private DelegateActionDelegate action { get; set; }
+
+        public DelegateAction(DelegateActionDelegate action)
+        {
+            if (action == null) throw new ArgumentNullException("action");
+
+            this.condition = new DelegateConditionDelegate((trigger) => true);
+            this.action = action;
+        }
+
+        public DelegateAction(DelegateConditionDelegate condition, DelegateActionDelegate action)
+        {
+            if (condition == null) throw new ArgumentNullException("condition");
+            if (action == null) throw new ArgumentNullException("action");
+
+            this.condition = condition;
+            this.action = action;
+        }
+
+        public override Boolean Condition(JassTrigger trigger)
+        {
+            return this.condition(trigger);
+        }
+
+        public override void Action(JassTrigger trigger)
+        {
+            this.action(trigger);
+        }
+    }
+}
