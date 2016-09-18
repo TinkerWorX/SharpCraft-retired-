ff72ea0ab9c91135b682d8cdb4128e6f3456388f
ChiefOfGxBxL <chiefofgxbxl@github.com>
2016-09-18 16:42:00 -0400

    Formatting common.cs properties

--- TinkerWorX.SharpCraft.Launcher/Common.cs
+++ TinkerWorX.SharpCraft.Launcher/Common.cs
@@ -13,17 +13,22 @@ namespace TinkerWorX.SharpCraft.Launcher
 {
     internal static class Common
     {
-        public static bool IsInitialized { get; private set; }
+        public static Boolean IsInitialized
+        { get; private set; }
 
-        public static string InstallPath { get; private set; }
+        public static String InstallPath
+        { get; private set; }
 
-        public static string GamePath { get; private set; }
+        public static String GamePath
+        { get; private set; }
 
-        public static string EditorPath { get; private set; }
+        public static String EditorPath
+        { get; private set; }
 
-        public static string Version { get { return FileVersionInfo.GetVersionInfo(typeof(SharpCraftApplication).Assembly.Location).FileVersion; } }
+        public static String Version
+        { get { return FileVersionInfo.GetVersionInfo(typeof(SharpCraftApplication).Assembly.Location).FileVersion; } }
 
-        public static bool Initialize()
+        public static Boolean Initialize()
         {
             if (!Common.IsInitialized)
             {
@@ -77,7 +82,7 @@ namespace TinkerWorX.SharpCraft.Launcher
             return true;
         }
 
-        public static void StartGame(string[] args)
+        public static void StartGame(String[] args)
         {
             if (!Common.Initialize())
                 return;
