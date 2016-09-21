b5b95823223235eaa54f5397c156eecc7b724b6b
ChiefOfGxBxL <chiefofgxbxl@github.com>
2016-09-20 21:34:51 -0400

    Also reverting back to using aliases

--- TinkerWorX.SharpCraft.Launcher/Common.cs
+++ TinkerWorX.SharpCraft.Launcher/Common.cs
@@ -13,17 +13,17 @@ namespace TinkerWorX.SharpCraft.Launcher
 {
     internal static class Common
     {
-        public static Boolean IsInitialized { get; private set; }
+        public static bool IsInitialized { get; private set; }
 
-        public static String InstallPath { get; private set; }
+        public static string InstallPath { get; private set; }
 
-        public static String GamePath { get; private set; }
+        public static string GamePath { get; private set; }
 
-        public static String EditorPath { get; private set; }
+        public static string EditorPath { get; private set; }
 
-        public static String Version { get { return FileVersionInfo.GetVersionInfo(typeof(SharpCraftApplication).Assembly.Location).FileVersion; } }
+        public static string Version { get { return FileVersionInfo.GetVersionInfo(typeof(SharpCraftApplication).Assembly.Location).FileVersion; } }
 
-        public static Boolean Initialize()
+        public static bool Initialize()
         {
             if (!Common.IsInitialized)
             {
@@ -77,7 +77,7 @@ namespace TinkerWorX.SharpCraft.Launcher
             return true;
         }
 
-        public static void StartGame(String[] args)
+        public static void StartGame(string[] args)
         {
             if (!Common.Initialize())
                 return;
